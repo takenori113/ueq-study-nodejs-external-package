4a4b50b3214db8d8c409bf753f114da6c509558b
Ken Takahashi <takaken@redimpulz.com>
2023-12-23 22:22:25 +0900

    タイポを修正

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -13,13 +13,13 @@ describe("外部パッケージの問題1(csv-parse)", () => {
     expect(spy).toHaveLastReturnedWith("国語の平均点: 77.5点");
   });
   test("成績表の点数を変えても結果が正しい", async () => {
-    await cp("./__test__/seiseki_other.csv", "./seiseki.csv", { force: true });
+    await cp("./__tests__/seiseki_other.csv", "./seiseki.csv", { force: true });
     const spy = vi
       .spyOn(global.console, "log")
       .mockImplementation((args) => args);
     await main1();
     expect(spy).toHaveLastReturnedWith("国語の平均点: 80.7点");
-    await cp("./__test__/seiseki.csv", "./seiseki.csv", { force: true });
+    await cp("./__tests__/seiseki.csv", "./seiseki.csv", { force: true });
   });
 });
 
@@ -27,7 +27,7 @@ describe("外部パッケージの問題2(sharp)", () => {
   test("問題2の結果が正しい", async () => {
     await rm("./output.png").catch(() => {});
     await main2();
-    const answer = await readFile("./__test__/answer.png", "base64");
+    const answer = await readFile("./__tests__/answer.png", "base64");
     const result = await readFile("./output.png", "base64");
     expect(answer).toEqual(result);
     await rm("./output.png");
