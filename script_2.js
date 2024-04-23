ce345332ceb59b6807fd4d384eb3f047ca5efb07
Takenori <takenori113@gmail.com>
2024-04-23 20:52:55 +0900

    returnの追記

--- script_2.js
+++ script_2.js
@@ -4,9 +4,9 @@ export const main = async () => {
   const imgPath = "./logo.png";
   const outputPath = "./output.png";
 
-  await sharp(imgPath)
-    .resize(100)
-    .grayscale()
-    .toFile(outputPath);
+  return sharp(imgPath)
+  .resize(100)
+  .grayscale()
+  .toFile(outputPath)
 }
 main();
